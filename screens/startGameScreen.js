b23b16f1dd3dfbbe22064b71e6839c7e0502cd58
Ateet Tiwari <ateettiwari@Ateets-MacBook-Pro.local>
2020-03-02 19:19:12 +0530

    added general component layout

--- screens/startGameScreen.js
+++ screens/startGameScreen.js
@@ -1,23 +1,29 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  Button
-} from "react-native";
-import Color from '../constants/color'
-import Card from '../components/card'
+import { View, Text, StyleSheet, Button } from "react-native";
+import Color from "../constants/color";
+import Card from "../components/card";
+import Input from "../components/input";
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrent={false}
+          keyboardType="number-pad"
+          maxLength={2}
+        />
         <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Color.accent}/></View>
-          <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Color.primary}/></View>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Color.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color={Color.primary} />
+          </View>
         </View>
       </Card>
     </View>
@@ -45,9 +51,12 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: 15
   },
-  button:{
-      width: 100,
-
+  button: {
+    width: 100
+  },
+  input: {
+    width: 50,
+    textAlign: "center"
   }
 });
 
