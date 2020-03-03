879ece69691d0efa304d7f4311e523ff6507fa7a
Ateet Tiwari <ateettiwari@Ateets-MacBook-Pro.local>
2020-03-03 12:34:39 +0530

    added game login with useRef property

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/header'
 import StartGameScreen from './screens/startGameScreen'
+import GameScreen from './screens/gameScreen'
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  }
+
+  let content = <StartGameScreen onStartGame={startGameHandler}/>
+  if (userNumber){
+    content = <GameScreen userChoice={userNumber}/>
+  }
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number"/>
-      <StartGameScreen/>
+    {content} 
     </View>
   );
 }
